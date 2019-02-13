d7265b1bed8742d0bc9a2b76ab0a8c8040c8c837
mico <mi-const@yandex.ru>
2019-02-13 11:45:03 +0300

    initial page setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule, Type } from "@angular/core";
+import { Routes, RouterModule, Route } from "@angular/router";
 
-const routes: Routes = [];
+import { HomeComponent } from "./home/home.component";
+import { WorkoutsComponent } from "./workouts/workouts.component";
+import { EntryEditorComponent } from "./entry-editor/entry-editor.component";
+
+const _R_ = (path: string, component?: Type<any>): Route => ({
+  path,
+  component
+});
+
+const routes: Routes = [
+  _R_("", HomeComponent),
+  _R_("workouts", WorkoutsComponent),
+  _R_("workouts/:id", EntryEditorComponent)
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
