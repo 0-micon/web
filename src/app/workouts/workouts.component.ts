52e2c2754f311a38ef9886b8608c6e4c510ebf01
mico <mi-const@yandex.ru>
2019-02-16 13:18:36 +0300

    modal basic functionality

--- src/app/workouts/workouts.component.ts
+++ src/app/workouts/workouts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { WorkoutsApiService } from "../services/workouts-api.service";
+import { NgbModal, NgbModalOptions } from "@ng-bootstrap/ng-bootstrap";
 
 //import * as _ from "lodash";
 
@@ -19,22 +20,35 @@ export class WorkoutsComponent implements OnInit {
   loading: boolean = false;
   workouts = [];
 
-  constructor(private api: WorkoutsApiService) {}
+  constructor(private api: WorkoutsApiService, private modal: NgbModal) {}
 
   ngOnInit() {
     this.loading = true;
-    this.api.getWorkouts().subscribe(data => {
-      this.workouts = (data as unknown) as Entry[];
-      this.loading = false;
-    }, err => {
-      this.loading = false;
-    });
+    this.api.getWorkouts().subscribe(
+      data => {
+        this.workouts = (data as unknown) as Entry[];
+        this.loading = false;
+      },
+      err => {
+        this.loading = false;
+      }
+    );
   }
 
-  deleteWorkout(id: number) {
-    this.api.deleteWorkout(id).subscribe(data => {
-      this.workouts = this.workouts.filter(item => item.id !== id);
-      //_.remove(this.workouts, { id });
-    });
+  deleteWorkout(id: number, modalTemplate) {
+    const options: NgbModalOptions = {
+      size: 'sm'
+    }
+    this.modal.open(modalTemplate, options).result.then(
+      result => {
+        this.api.deleteWorkout(id).subscribe(data => {
+          this.workouts = this.workouts.filter(item => item.id !== id);
+          //_.remove(this.workouts, { id });
+        });
+      },
+      reason => {
+        console.log("Dismissed: " + reason);
+      }
+    );
   }
 }
