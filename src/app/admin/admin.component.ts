b5341bdef1128e73b574c79e6f51e23fa0a24912
mico <mi-const@yandex.ru>
2019-02-16 16:46:25 +0300

    carousel

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  selector: "app-admin",
+  templateUrl: "./admin.component.html",
+  styleUrls: ["./admin.component.css"]
 })
 export class AdminComponent implements OnInit {
+  images = [
+    { src: "assets/images/hunt.jpg", title: "Hunting Area" },
+    { src: "assets/images/husky.jpg", title: "Siberian Husky" },
+    { src: "assets/images/io.jpg", title: "Io" },
+    { src: "assets/images/pears-soap.jpg", title: "Pears" },
+    { src: "assets/images/tom-and-jerry.jpg", title: "Tom and Jerry" },
+    { src: "assets/images/trees.jpg", title: "Trees" },
+    { src: "assets/images/yoyos.jpg", title: "Yoyos" }
+  ];
 
-  constructor() { }
+  @ViewChild("imgCarousel")
+  imgCarousel: any;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  prev() {
+    this.imgCarousel.prev();
   }
 
+  next() {
+    this.imgCarousel.next();
+  }
 }
