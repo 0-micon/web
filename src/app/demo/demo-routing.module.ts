8c906e6aea3476bd6199221f1caa45477317567a
0-micon <0.miconst@gmail.com>
2019-03-06 10:09:59 +0300

    Flexbox Demo

--- src/app/demo/demo-routing.module.ts
+++ src/app/demo/demo-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { ButtonsComponent } from "./buttons/buttons.component";
+import { FlexboxComponent } from "./flexbox/flexbox.component";
 
 const routes: Routes = [
   { path: "buttons", component: ButtonsComponent },
+  { path: "flex", component: FlexboxComponent },
   { path: "**", redirectTo: "buttons" }
 ];
 
--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { MaterialModule } from "../shared/material.module";
 
 import { DemoRoutingModule } from "./demo-routing.module";
 import { ButtonsComponent } from "./buttons/buttons.component";
+import { FlexboxComponent } from './flexbox/flexbox.component';
 
 @NgModule({
-  declarations: [ButtonsComponent],
-  imports: [CommonModule, DemoRoutingModule, FormsModule, MaterialModule]
+  declarations: [ButtonsComponent, FlexboxComponent],
+  imports: [
+    CommonModule,
+    DemoRoutingModule,
+    FormsModule,
+    FlexLayoutModule,
+    MaterialModule
+  ]
 })
 export class DemoModule {}
