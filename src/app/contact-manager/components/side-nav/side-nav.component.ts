7bec33e325c2b25acd6102c736a3adda1622dcf9
0-micon <0.miconst@gmail.com>
2019-03-07 10:16:55 +0300

    Adding Responsiveness

--- src/app/contact-manager/components/side-nav/side-nav.component.ts
+++ src/app/contact-manager/components/side-nav/side-nav.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from "@angular/core";
+
+const SMALL_SCREEN_WIDTH_QUERY = "(max-width: 720px)";
 
 @Component({
-  selector: 'app-side-nav',
-  templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  selector: "app-side-nav",
+  templateUrl: "./side-nav.component.html",
+  styleUrls: ["./side-nav.component.scss"]
 })
 export class SideNavComponent implements OnInit {
+  private mediaMatcher: MediaQueryList;
 
-  constructor() { }
+  isSmallScreen: boolean;
+
+  constructor(private zone: NgZone) {}
 
   ngOnInit() {
-  }
+    this.mediaMatcher = matchMedia(SMALL_SCREEN_WIDTH_QUERY);
+    this.isSmallScreen = this.mediaMatcher.matches;
 
+    this.mediaMatcher.addListener(mql =>
+      this.zone.run(() => (this.isSmallScreen = mql.matches))
+    );
+  }
 }
