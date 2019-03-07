fe81022cacb76e164469618dade9406f22bc063f
0-micon <0.miconst@gmail.com>
2019-03-07 09:39:28 +0300

    Styling the Sidenav

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -2,18 +2,29 @@ import { NgModule } from "@angular/core";
 import {
   MatButtonModule,
   MatCheckboxModule,
-  MatIconModule
+  MatIconModule,
+  MatListModule
 } from "@angular/material";
 
 import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatCheckboxModule,
     MatIconModule,
-    MatSidenavModule
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
-  exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatSidenavModule]
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule
+  ]
 })
 export class MaterialModule {}
