99c143aa093cc2a47bc27ad70af072f0bbb91b8f
mico <mi-const@yandex.ru>
2019-03-13 17:36:08 +0300

    adding bootstrap css styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `
+    <div>
+      <h1>{{ title }}</h1>
+      <div>My First Component</div>
+    </div>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
