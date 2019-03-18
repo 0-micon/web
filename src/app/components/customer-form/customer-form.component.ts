07ec2e470deba0a15592de30e9cc0e242c6cfbe4
mico <mi-const@yandex.ru>
2019-03-18 18:38:57 +0300

    Setting Built-in Validation Rules

--- src/app/components/customer-form/customer-form.component.ts
+++ src/app/components/customer-form/customer-form.component.ts
@@ -4,8 +4,51 @@ import {
   FormGroup,
   FormControl,
   AbstractControl,
-  FormBuilder
+  FormBuilder,
+  ValidationErrors,
+  Validators
 } from '@angular/forms';
+
+// class FormBase {
+//   mainForm: FormGroup;
+
+//   isControlValid(controlName: string): boolean {
+//     const control = this.mainForm.get(controlName);
+//     return control.valid || !(control.touched || control.dirty);
+//   }
+
+//   getErrors(controlName: string): any {
+//     return this.mainForm.get(controlName).errors;
+//   }
+
+//   constructor(controls: string[] = []) {
+//     for (const control of controls) {
+//       this.defineControlGetters(control);
+//     }
+//   }
+
+//   defineControlGetters(controlName: string): void {
+//     Object.defineProperty(this, controlName, {
+//       get(): AbstractControl {
+//         return this.mainForm.get(controlName);
+//       }
+//     });
+//     Object.defineProperty(this, `${controlName}Errors`, {
+//       get(): any {
+//         return this[controlName].errors;
+//       }
+//     });
+
+//     const uName =
+//       controlName.substring(0, 1).toUpperCase() + controlName.substring(1);
+//     Object.defineProperty(this, `is${uName}Valid`, {
+//       get(): boolean {
+//         const control = this[controlName];
+//         return control.valid || !(control.touched || control.dirty);
+//       }
+//     });
+//   }
+// }
 
 @Component({
   selector: 'app-customer-form',
@@ -13,18 +56,36 @@ import {
   styleUrls: ['./customer-form.component.scss']
 })
 export class CustomerFormComponent implements OnInit {
-  customerForm: FormGroup;
+  mainForm: FormGroup;
   customer = new Customer();
 
-  constructor(private fb: FormBuilder) {}
+  isControlValid(controlName: string): boolean {
+    const control = this.mainForm.get(controlName);
+    return control.valid || !(control.touched || control.dirty);
+  }
+
+  getErrors(controlName: string): any {
+    return this.mainForm.get(controlName).errors;
+  }
+
+  constructor(private fb: FormBuilder) {
+    // super(['firstName', 'lastName', 'email', 'sendCatalog']);
+  }
 
   ngOnInit() {
-    this.customerForm = this.fb.group({
-      firstName: '',
-      lastName: '',
-      email: '',
+    this.mainForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
       sendCatalog: true
     });
+
+    // Object.defineProperty(this, 'isEmailValid', {
+    //   get(): boolean {
+    //     const control = this.email;
+    //     return control.valid || !(control.touched || control.dirty);
+    //   }
+    // });
 
     // this.customerForm = new FormGroup({
     //   firstName: new FormControl(),
@@ -35,28 +96,46 @@ export class CustomerFormComponent implements OnInit {
   }
 
   get formValid(): boolean {
-    return this.customerForm && this.customerForm.valid;
+    return this.mainForm && this.mainForm.valid;
   }
 
-  get firstName(): AbstractControl {
-    return this.customerForm.get('firstName');
-  }
+  // get firstName(): AbstractControl {
+  //   return this.mainForm.get('firstName');
+  // }
 
-  get lastName(): AbstractControl {
-    return this.customerForm.get('lastName');
-  }
+  // get firstNameErrors(): any {
+  //   return this.firstName.errors;
+  // }
 
-  get email(): AbstractControl {
-    return this.customerForm.get('email');
-  }
+  // get isFirstNameValid(): boolean {
+  //   const control = this.firstName;
+  //   return control.valid || !(control.touched || control.dirty);
+  // }
+
+  // get lastName(): AbstractControl {
+  //   return this.mainForm.get('lastName');
+  // }
+
+  // get lastNameErrors(): any {
+  //   return this.lastName.errors;
+  // }
+
+  // get isLastNameValid(): boolean {
+  //   const control = this.lastName;
+  //   return control.valid || !(control.touched || control.dirty);
+  // }
+
+  // get email(): AbstractControl {
+  //   return this.mainForm.get('email');
+  // }
 
   save() {
-    console.log('Form:', this.customerForm);
-    console.log('Saved:' + JSON.stringify(this.customerForm.value));
+    console.log('Form:', this.mainForm);
+    console.log('Saved:' + JSON.stringify(this.mainForm.value));
   }
 
   populateTestData(): void {
-    this.customerForm.patchValue({
+    this.mainForm.patchValue({
       firstName: 'Fame',
       lastName: 'Lame',
       email: 'fame@lame.com',
