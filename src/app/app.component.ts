aee4adaf71c2b4543a343dad2b30b5e6c1e6f918
mico <mi-const@yandex.ru>
2019-03-29 11:35:06 +0300

    Reacting to Routing Events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { AppAnimation } from './app.animation';
+import {
+  Router,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +18,25 @@ export class AppComponent {
   title = 'demo-app';
   isLoggedIn: boolean = false;
   userName: string;
+  loading: boolean = true;
 
   logOut() {
     this.isLoggedIn = false;
     this.userName = '';
+  }
+
+  constructor(private _router: Router) {
+    _router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.loading = true;
+      }
+      if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationError ||
+        event instanceof NavigationCancel
+      ) {
+        this.loading = false;
+      }
+    });
   }
 }
