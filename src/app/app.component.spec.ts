7f69710b37ba0f907da88b8e20f016583dd65154
mico <mi-const@yandex.ru>
2019-05-01 12:10:10 +0300

    Create Project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'workout-tracker'`, () => {
+  it(`should have as title 'books'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('workout-tracker');
+    expect(app.title).toEqual('books');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to workout-tracker!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to books!');
   });
 });
